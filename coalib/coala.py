71667e082853b19ca1b29b8d0f7adbb9939076fb
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2019-02-07 16:19:25 +0530

    Test change

--- coalib/coala.py
+++ coalib/coala.py
@@ -9,7 +9,7 @@
 # for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. Just a small change.
 
 import logging
 import sys
